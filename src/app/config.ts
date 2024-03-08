e998a7c861022df2f99e1d85564ea753b40e6f18
Chris <chris35469@gmail.com>
2024-03-08 15:33:53 -0600

    Update API URL

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 // use NODE_ENV to not have to change config based on where it's deployed https://zizzamia.xyz
 export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3000/runners' : 'https://w3bbie.xyz/runners';
-export const API_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3001' : 'https://testapi35.netlify.app';
+export const API_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3001' : 'https://framerunnner.netlify.app';

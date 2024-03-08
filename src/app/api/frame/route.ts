f4d0087530aedf264940474a79623235d954907b
Chris <chris35469@gmail.com>
2024-03-07 20:00:33 -0600

    Update allowFramegear option to false

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -6,7 +6,7 @@ import { getData } from '../../utils/firebase'
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
     const body: FrameRequest = await req.json();
-    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT', allowFramegear: true });
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT', allowFramegear: false });
 
     let { raceState, isRacing } = await getData()
     console.log(raceState)
